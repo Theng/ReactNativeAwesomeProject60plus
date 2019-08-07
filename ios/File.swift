ab003e717065527ea08f4022c122b8a9e0296142
Cheng Chungtheng <cheng.chungtheng@sabay.com>
2019-08-07 11:38:19 +0700

    Add Lottie library

---
+++ ios/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  AwesomeProject
+//
+//  Created by Cheng Chungtheng on 8/7/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+import Foundation
--- src/containers/home/home.js
+++ src/containers/home/home.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { View, Text  } from "react-native";
 import Header from "../../components/MainHeader"
+import LottieView from 'lottie-react-native';
 
 class HomeScreen extends Component {
     constructor(props) {
@@ -17,6 +18,7 @@ class HomeScreen extends Component {
             <View style={{flex:1}}>
                 <Header/>
                 <Text>Home</Text>
+                <LottieView source={require('../../assets/8252-looking-for-jobs.json')} autoPlay loop />
             </View>
         );
     }
